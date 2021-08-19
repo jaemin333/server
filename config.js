3992f6a9a77ce6251d013577e49bd60ac6761209
jaemin333 <81303312+jaemin333@users.noreply.github.com>
2021-08-20 01:56:26 +0900

    first commit

--- config.js
+++ config.js
@@ -28,5 +28,6 @@ export const config = {
         user: required('DB_USER'),
         database: required('DB_DATABASE'),
         password: required('DB_PASSWORD'),
+        port:required('DB_PORT'),
     },
 };
